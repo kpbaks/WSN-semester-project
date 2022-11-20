ba1c962b7e45302f4e11a41a07a0811df6a870a5
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2022-11-20 13:50:57 +0100

    feat: broadcast counter

--- nullnet-sender/sender.c
+++ nullnet-sender/sender.c
@@ -18,41 +18,41 @@ PROCESS_THREAD(main_process, ev, data)
 
 	static struct etimer et;
 
-	uint32_t counter = 0;
+	static uint32_t counter = 0;
 
 	// Set the timer to 2 seconds
 	etimer_set(&et, CLOCK_SECOND * 2);
 
 	while (1)
-	{
-		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		{
+			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+			// uint8_t payload[64] = { 0 };
+
+			// char *msg = "hello world: ";
+			// // sprintf(payload, "%s %u", msg, counter);
+			// ++counter;
+
+			// memcpy(payload, msg, strlen(msg));
+			// memcpy(payload + strlen(msg), &counter, sizeof(counter));
+
+			uint8_t payload[4] = { 0 };
+			memcpy(payload, &counter, sizeof(counter));
+			++counter;
+
+			nullnet_buf =  payload; /* Point NullNet buffer to 'payload' */
+			nullnet_len = sizeof(payload); /* Set the length of the NullNet buffer to the length of 'payload' */
+
+			// nullnet_len = strlen(msg) + sizeof(counter); /* Set payload length */
 
 
-	uint8_t payload[64] = { 0 };
-			
-	char *msg = "msg: ";
-	sprintf(payload, "%s %d", msg, counter++);
-	memcpy(payload, msg, strlen(msg));
+			NETSTACK_NETWORK.output(NULL); // send as broadcast
+			LOG_INFO("Sent: %s (len=%d) via NullNet network layer\n", payload, nullnet_len); 
 
-
-	nullnet_buf = payload; /* Point NullNet buffer to 'payload' */
-	// nullnet_len = 2; /* Tell NullNet that the payload length is two bytes */
-	nullnet_len = strlen(msg);
-	NETSTACK_NETWORK.output(NULL); /* Send as broadcast */
-	LOG_INFO("Sent: %s (len=%d) via NullNet network layer\n", msg, nullnet_len); 
-
-	etimer_reset(&et);
+			etimer_reset(&et);
 
 	}
-	
 
-  PROCESS_END();
+	PROCESS_END();
 }
-
-
-
-
-
- 
-
 
